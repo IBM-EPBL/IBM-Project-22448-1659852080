4c35ae20f9ce192f9918d19bad274f759fba7be9
Nithin2830 <nithin2830@gmail.com>
2022-10-17 22:53:55 +0530

    uploaded

--- Assignments/3.NANDHINI R/Assignment 3/Assignment_3.py
+++ Assignments/3.NANDHINI R/Assignment 3/Assignment_3.py
@@ -1,32 +1,32 @@
+//python code for blinking LED
 import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print("LED off")
-GPIO.output(18,GPIO.LOW)
-#Traffic Light
-from gpiozero import Button, TrafficLights, Buzzer
-from time import sleep
-buzzer = Buzzer(15)
-button = Button(21)
-lights = TrafficLights(25, 8, 7)
-#Button 21
-#Red LED 25
-#Yellow LED 08
-#Green LED 07
-#Buzzer 15
+GPIO.setup(2,GPIO.OUT)
 while True:
-  button.wait_for_press()
-  buzzer.on()
-  light.green.on()
-  sleep(1)
-  lights.amber.on()
-  sleep(1)
-  lights.red.on()
-  sleep(1)
-  lights.off()
-  buzzer.off()
+ GPIO.output(2,True)
+ time.sleep(2)
+ GPIO.output(2,False)
+ time.sleep(2)
+
+
+//Python code for Traffic Lights
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+while True:
+ print "RED SIGNAL"
+ GPIO.output(2,True)
+ time.sleep(2)
+ GPIO.output(2,False)
+ print "YELLOW SIGNAL"
+ GPIO.output(17,True)
+ time.sleep(2)
+ GPIO.output(17,False)
+ print "GREEN SIGNAL"
+ print(GPIO.output(4,True))
+ time.sleep(2)
+ GPIO.output(4,False)
